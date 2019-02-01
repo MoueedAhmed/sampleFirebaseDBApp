d24ff90250d87ffe6e96cb68f3dd9e6ee8f6888e
AMOUEED <moueedrao@gmail.com>
2019-02-02 01:50:55 +0500

    Complete RegistrationActivity and Add HomeActivity with default Layout

--- app/src/main/java/com/example/samplefirebasedbapp/MainActivity.java
+++ app/src/main/java/com/example/samplefirebasedbapp/MainActivity.java
@@ -1,13 +1,24 @@
 package com.example.samplefirebasedbapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private TextView registerTxt;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        registerTxt = (TextView) findViewById(R.id.registerTxt_login);
+        registerTxt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(),RegistrationActivity.class));
+            }
+        });
     }
 }
