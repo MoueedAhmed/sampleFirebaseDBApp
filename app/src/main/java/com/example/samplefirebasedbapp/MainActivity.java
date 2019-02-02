47de7ef8179a01c99b08dc16027ed00290ef984b
AMOUEED <moueedrao@gmail.com>
2019-02-02 14:54:18 +0500

    Complete firebase auth in MainActivity

--- app/src/main/java/com/example/samplefirebasedbapp/MainActivity.java
+++ app/src/main/java/com/example/samplefirebasedbapp/MainActivity.java
@@ -1,23 +1,79 @@
 package com.example.samplefirebasedbapp;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
 
     private TextView registerTxt;
+    private EditText email;
+    private EditText password;
+    private Button loginBtn;
+    private FirebaseAuth mAuth;
+    private ProgressDialog mDialog;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         registerTxt = (TextView) findViewById(R.id.registerTxt_login);
+        mAuth = FirebaseAuth.getInstance();
+        mDialog = new ProgressDialog(this);
+        email = (EditText) findViewById(R.id.email_login);
+        password = (EditText) findViewById(R.id.password_login);
+        loginBtn = (Button) findViewById(R.id.loginBtn);
+
         registerTxt.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 startActivity(new Intent(getApplicationContext(),RegistrationActivity.class));
+            }
+        });
+
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String mEmail = email.getText().toString().trim();
+                String mPassword = password.getText().toString().trim();
+
+                if(TextUtils.isEmpty(mEmail)){
+                    email.setError("Email Required...");
+                }
+                if(TextUtils.isEmpty(mPassword)){
+                    password.setError("Password Required...");
+                }
+
+                mDialog.setMessage("Processing...");
+                mDialog.show();
+
+                mAuth.signInWithEmailAndPassword(mEmail,mPassword).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            Toast.makeText(getApplicationContext(),"Sucessfull",Toast.LENGTH_LONG).show();
+                            startActivity(new Intent(getApplicationContext(),HomeActivity.class));
+                            mDialog.dismiss();
+                        }
+                        else {
+                            Toast.makeText(getApplicationContext(),"Problem in Auth",Toast.LENGTH_LONG).show();
+                            mDialog.dismiss();
+                        }
+                    }
+                });
             }
         });
     }
