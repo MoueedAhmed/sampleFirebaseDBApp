38761fb5ca4343a42a7d204ad4f3f259bb957a3b
AMOUEED <moueedrao@gmail.com>
2019-02-02 18:04:21 +0500

    Add Insert Data to Firebase Database Feature

--- app/src/main/java/com/example/samplefirebasedbapp/HomeActivity.java
+++ app/src/main/java/com/example/samplefirebasedbapp/HomeActivity.java
@@ -4,15 +4,38 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.example.samplefirebasedbapp.Model.Data;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.text.DateFormat;
+import java.util.Date;
 
 public class HomeActivity extends AppCompatActivity {
     private FloatingActionButton fabAdd;
+
+    private DatabaseReference mDatabase;
+    private FirebaseAuth mAuth;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        mAuth = FirebaseAuth.getInstance();
+        FirebaseUser mUser = mAuth.getCurrentUser();
+        String uId = mUser.getUid();
+
+        mDatabase = FirebaseDatabase.getInstance().getReference().child("TaskNote").child(uId);
 
         fabAdd = (FloatingActionButton) findViewById(R.id.addFab_home);
         fabAdd.setOnClickListener(new View.OnClickListener() {
@@ -22,7 +45,34 @@ public class HomeActivity extends AppCompatActivity {
                 LayoutInflater inflater = LayoutInflater.from(HomeActivity.this);
                 View myView = inflater.inflate(R.layout.custom_input,null);
                 myDialog.setView(myView);
-                AlertDialog dialog = myDialog.create();
+                final AlertDialog dialog = myDialog.create();
+
+                final EditText title = myView.findViewById(R.id.title_custom_input);
+                final EditText note = myView.findViewById(R.id.note_custom_input);
+                Button saveBtn = myView.findViewById(R.id.saveBtn_custom_input);
+
+                saveBtn.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        String mTitle = title.getText().toString().trim();
+                        String mNote = note.getText().toString().trim();
+
+                        if(TextUtils.isEmpty(mTitle)){
+                            title.setError("Title is required!");
+                        }
+                        if(TextUtils.isEmpty(mNote)){
+                            note.setError("Note is required!");
+                        }
+
+                        String id = mDatabase.push().getKey();
+                        String date = DateFormat.getDateInstance().format(new Date());
+                        Data data = new Data(mTitle,mNote,date,id);
+                        mDatabase.child(id).setValue(data);
+                        Toast.makeText(getApplicationContext(),"Data Inserted",Toast.LENGTH_SHORT).show();;
+                        dialog.dismiss();
+                    }
+                });
+
                 dialog.show();
             }
         });
