7af4477fda141614612002943a46476ca4ea8790
AMOUEED <moueedrao@gmail.com>
2019-02-02 16:49:05 +0500

    Create custom_input.xml and add Custom dialog in HomeActivity when fab is clicked

--- app/src/main/java/com/example/samplefirebasedbapp/HomeActivity.java
+++ app/src/main/java/com/example/samplefirebasedbapp/HomeActivity.java
@@ -1,8 +1,10 @@
 package com.example.samplefirebasedbapp;
 
 import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
 
 public class HomeActivity extends AppCompatActivity {
@@ -16,7 +18,12 @@ public class HomeActivity extends AppCompatActivity {
         fabAdd.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                AlertDialog.Builder myDialog = new AlertDialog.Builder(HomeActivity.this);
+                LayoutInflater inflater = LayoutInflater.from(HomeActivity.this);
+                View myView = inflater.inflate(R.layout.custom_input,null);
+                myDialog.setView(myView);
+                AlertDialog dialog = myDialog.create();
+                dialog.show();
             }
         });
     }
