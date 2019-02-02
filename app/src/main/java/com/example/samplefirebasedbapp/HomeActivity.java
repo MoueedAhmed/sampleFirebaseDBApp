e6be3ee265bedc8c474c5b72be315447170f0458
AMOUEED <moueedrao@gmail.com>
2019-02-02 15:58:13 +0500

    Add CoordinatorLayout and FloatingActionButton in activity_home.xml

--- app/src/main/java/com/example/samplefirebasedbapp/HomeActivity.java
+++ app/src/main/java/com/example/samplefirebasedbapp/HomeActivity.java
@@ -1,13 +1,23 @@
 package com.example.samplefirebasedbapp;
 
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class HomeActivity extends AppCompatActivity {
-
+    private FloatingActionButton fabAdd;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        fabAdd = (FloatingActionButton) findViewById(R.id.addFab_home);
+        fabAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
 }
